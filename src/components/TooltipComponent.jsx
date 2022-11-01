9d6e72d49c348ab56accfc33bd9c737cae9baaf1
Decxo360 <diegolundstedt99@gmail.com>
2022-11-01 18:57:22 -0300

    cambio de orientacion

--- src/components/TooltipComponent.jsx
+++ src/components/TooltipComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const TooltipComponent = ({children}) => {
   return (
-    <div className='tooltip flex flex-row justify-center items-center xs'>
+    <div className='tooltip flex flex-col justify-center items-center xs'>
         {children}
     </div>
   )
