fd98d7d7943d5cb0317876be90b71aeabfb28798
Decxo360 <diegolundstedt99@gmail.com>
2022-11-01 18:55:24 -0300

    modificar url de las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ import { RedesSociales } from './components/RedesSociales'
 
 
 const proyectos = [
-  {id:1,titulo:'JournalApp', codigo:'https://github.com/Decxo360/JournalApp', etiquetas:['yarn','vite','react','react-redux','redux-toolkit','firebase','social Login','fetch','Javascript','Auth','Material UI','Swet Alert 2','react rooter dom'],descripcion:'Esta es una aplicacion de notas donde puedes guardar tu nota actual, subir imagenes de lo que haz hecho, eliminar notas, crear, actualizar. Cuenta con login local y social login de google.',url:'../../../public/proyecto1.png'},
-  {id:2,titulo:'Buscador de Gifs', codigo:'https://github.com/Decxo360/react-gif-expertApp', etiquetas:['npm','react','fetch'], descripcion:'Proyecto donde consumo una api rest de Giphy, la cual consiste en buscar el gif relacionado con lo escrito por el usuario',url:'../../../public/proyecto2.png'},
-  {id:3,titulo:'Ricks And Mortys',codigo:'https://github.com/Decxo360/RicksAndMorty-s',etiquetas:['yarn','vite','react-redux','react','RTQuerys','bootstrap','react router dom'],descripcion:'Una web que permite buscar y ver las descripciones de los personajes de ricks and mortys la serie. Esto implementando una tecnologia la cual proviene de las RTQuerys',url:'../../../public/proyecto 3.png'},
-  {id:4,titulo:'Indecisions',codigo:'sin codigo',etiquetas:['yarn','vue','fetch'],descripcion:'Una web la cual consumo una api res, la cual retorna un gif el cual responde si o no a la pregunta ingresada por el usuario',url:'../../../public/proyecto4.png'},
-  {id:5,titulo:'Backend node js',codigo:'https://github.com/Decxo360/blog-app-mysql-nodejs',etiquetas:['yarn','node','express','mysql','passport','jwt','brcyrpts','dotenv','nodemon','CRUD','express-session'],descripcion:'Este es un backend hecho com mysqq, workbench, xampp. Es un backend hecho para una pagina web futura la cual constara de creacion de publicaciones, perfil, entre otras cosas. Por curiosidad implemente este backend tanto en java+springboot y tambien en laravel+php',url:'../../../public/proyecto5.png'}
+  {id:1,titulo:'JournalApp', codigo:'https://github.com/Decxo360/JournalApp', etiquetas:['yarn','vite','react','react-redux','redux-toolkit','firebase','social Login','fetch','Javascript','Auth','Material UI','Swet Alert 2','react rooter dom'],descripcion:'Esta es una aplicacion de notas donde puedes guardar tu nota actual, subir imagenes de lo que haz hecho, eliminar notas, crear, actualizar. Cuenta con login local y social login de google.',url:'https://res.cloudinary.com/drp7cwb8s/image/upload/v1666734353/proyecto1_pngnuf.png'},
+  {id:2,titulo:'Buscador de Gifs', codigo:'https://github.com/Decxo360/react-gif-expertApp', etiquetas:['npm','react','fetch'], descripcion:'Proyecto donde consumo una api rest de Giphy, la cual consiste en buscar el gif relacionado con lo escrito por el usuario',url:'https://res.cloudinary.com/drp7cwb8s/image/upload/v1666734358/proyecto2_pqpkeo.png'},
+  {id:3,titulo:'Ricks And Mortys',codigo:'https://github.com/Decxo360/RicksAndMorty-s',etiquetas:['yarn','vite','react-redux','react','RTQuerys','bootstrap','react router dom'],descripcion:'Una web que permite buscar y ver las descripciones de los personajes de ricks and mortys la serie. Esto implementando una tecnologia la cual proviene de las RTQuerys',url:'https://res.cloudinary.com/drp7cwb8s/image/upload/v1666734352/proyecto_3_h0l7go.png'},
+  {id:4,titulo:'Indecisions',codigo:'sin codigo',etiquetas:['yarn','vue','fetch'],descripcion:'Una web la cual consumo una api res, la cual retorna un gif el cual responde si o no a la pregunta ingresada por el usuario',url:'https://res.cloudinary.com/drp7cwb8s/image/upload/v1666734363/proyecto4_jaqatu.png'},
+  {id:5,titulo:'Backend node js',codigo:'https://github.com/Decxo360/blog-app-mysql-nodejs',etiquetas:['yarn','node','express','mysql','passport','jwt','brcyrpts','dotenv','nodemon','CRUD','express-session'],descripcion:'Este es un backend hecho com mysqq, workbench, xampp. Es un backend hecho para una pagina web futura la cual constara de creacion de publicaciones, perfil, entre otras cosas. Por curiosidad implemente este backend tanto en java+springboot y tambien en laravel+php',url:'https://res.cloudinary.com/drp7cwb8s/image/upload/v1666734356/proyecto5_yrpj1b.png'}
 ]
 
 function App() {
@@ -49,7 +49,7 @@ function App() {
           <CardComponent key={id} titulo={titulo} codigo={codigo} etiquetas={etiquetas} descripcion={descripcion} url={url}/>
         ))}
       </div>
-      <div className='bg-slate-900 h-[200px]' ref={redesSosciales}>
+      <div className='bg-slate-900 h-[auto]' ref={redesSosciales}>
         <RedesSociales/>
       </div>
       <Footer/>
