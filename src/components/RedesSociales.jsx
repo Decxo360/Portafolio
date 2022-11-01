f8d37752e29aaad4a9bfe2ebdda5844157f7da6c
Decxo360 <diegolundstedt99@gmail.com>
2022-11-01 18:57:08 -0300

    nuevos estilos y futuras mejoras

--- src/components/RedesSociales.jsx
+++ src/components/RedesSociales.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Github } from './svg/Github'
 import { Gmail } from './svg/Gmail'
 import { Linkedin } from './svg/Linkedin'
 import { TooltipComponent } from './TooltipComponent'
 
-const urls = [{url:'diegolundstest99@gmail.com'},{url:'https://github.com/Decxo360'},{url:'https://www.linkedin.com/in/diego-lundstedt-5a583a22a/'}]
+const urls = [{url:'diegolundstest99@gmail.com'},{url:'https://github.com/Decxo360'},{url:'https://www.linkedin.com/in/diegolundstedt/'}]
 
 export const RedesSociales = () => {
+  const [msg, setMsg] = useState("Copiar")
+
   return (
     <div className='flex flex-col justify-center items-center'>
         <h1 className='mt-5 text-[30px] text-zinc-200'> ¿Dónde me encuentras? </h1>
-        <div className='flex flex-row justify-around items-center mt-7 w-[100%]'>
-          <div onClick={navigator.clipboard.writeText(urls[0].url)}>
+        <div className='flex flex-col justify-around items-center mt-7 w-[100%] 2xl:flex-row xl:flex-row lg:flex-row md:flex-row sm:flex-row'>
+          <div className='mt-3 mb-5 flex flex-col rounded-2xl justify-center items-center w-[150px] h-[150px] shadow-2xl shadow-zinc-600 hover:cursor-pointer hover:shadow-blue-700' onClick={navigator.clipboard.writeText(urls[0].url)}>
             <TooltipComponent>
               <Gmail/>
-              <span className='tooltipText'>Mi correo</span>
+              <span className='tooltipText'>{msg}</span>
             </TooltipComponent>
-          </div>
-           <a href={urls[1].url}>
+            <span className='text-slate-200'>Mi correo</span>
+          </div >
+           <a className=' mt-3 mb-5 flex flex-col rounded-2xl justify-center items-center w-[150px] h-[150px] shadow-2xl shadow-zinc-600 hover:shadow-blue-700' href={urls[1].url}>
             <TooltipComponent>
               <Github/>
-              <span className='tooltipText'>Mi github</span>
+              <span className='tooltipText'>Ir</span>
             </TooltipComponent>
+            <span className='text-slate-200'>Mi Github</span>
            </a>
-          <a href={urls[2].url}>
+          <a className='mt-3 mb-5 flex flex-col rounded-2xl justify-center items-center w-[150px] h-[150px] shadow-2xl shadow-zinc-600 hover:shadow-blue-700' href={urls[2].url}>
             <TooltipComponent>
               <Linkedin/>
-              <span className='tooltipText'>Mi Linkedin</span>
+              <span className='tooltipText'>Ir</span>
+              
             </TooltipComponent>
-            
+            <span className='text-slate-200'>Mi Linkedin</span>
           </a>
         </div>
     </div>
