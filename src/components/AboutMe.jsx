8f91f42ae822dfe29e2ea282ff40d7e531762da0
Decxo360 <diegolundstedt99@gmail.com>
2022-11-01 18:55:58 -0300

    eliminar tamaño sm con flex row a flex col

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,9 +5,9 @@ const descripcion = `!Hola!, Me Llamo Diego Ignacio Lundstedt Muñoz y tengo 22 
 
 export const AboutMe = () => {
   return (
-    <div className='flex flex-row justify-center items-center mt-10'>
+    <div className='flex flex-col justify-center items-center mt-10 2xl:flex-row xl:flex-row lg:flex-row md:flex-row'>
         <div className='ml-5 mr-10 mb-5 mt-5 '>
-            <img className='rounded-full border-solid border-cyan-800 border-2 mx-auto' src="../../../public/videollamada.png" alt="persona" />
+            <img className='rounded-full border-solid border-cyan-800 border-2 mx-auto' src="https://res.cloudinary.com/drp7cwb8s/image/upload/v1666734356/videollamada_evur9z.png" alt="persona" />
             <p className='text-center text-[25px] text-yellow-50'>{nombre}</p>
         </div>
         <div className='w-[50%]'>
