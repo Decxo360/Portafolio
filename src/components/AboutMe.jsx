dc6e0e51870e74aa52797d26cfa2f8cc4477eab3
Decxo360 <diegolundstedt99@gmail.com>
2022-11-24 19:12:43 -0300

    re ubicacion de texto

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,10 +5,9 @@ const descripcion = `!Hola!, Me Llamo Diego Ignacio Lundstedt Muñoz y tengo 22 
 
 export const AboutMe = () => {
   return (
-    <div className='flex flex-col justify-center items-center mt-10 2xl:flex-row xl:flex-row lg:flex-row md:flex-row'>
+    <div className='flex flex-col justify-center items-center pt-10 2xl:flex-row xl:flex-row lg:flex-row md:flex-row animate__animated  animate__backInUp'>
         <div className='ml-5 mr-10 mb-5 mt-5 '>
             <img className='rounded-full border-solid border-cyan-800 border-2 mx-auto' src="https://res.cloudinary.com/drp7cwb8s/image/upload/v1666734356/videollamada_evur9z.png" alt="persona" />
-            <p className='text-center text-[25px] text-yellow-50'>{nombre}</p>
         </div>
         <div className='w-[50%]'>
             <p className='text-white text-justify mr-5 '>{descripcion}</p>
