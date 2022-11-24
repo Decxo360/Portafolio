4dce56636eed81f6d5f3c474275b3a0b303ad3dc
Decxo360 <diegolundstedt99@gmail.com>
2022-11-23 21:31:58 -0300

    modifcacion completa de redes sociales, add styles

--- src/components/RedesSociales.jsx
+++ src/components/RedesSociales.jsx
@@ -1,51 +1,81 @@
-import React, { useEffect, useState } from 'react'
-import { useMemo } from 'react'
-import { Github } from './svg/Github'
-import { Gmail } from './svg/Gmail'
-import { Linkedin } from './svg/Linkedin'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+
+import { Cv, Github, Linkedin, Gmail } from './svg/index'
+
+
 import { TooltipComponent } from './TooltipComponent'
 
-const urls = [{url:'diegolundstest99@gmail.com'},{url:'https://github.com/Decxo360'},{url:'https://www.linkedin.com/in/diegolundstedt/'}]
+const urls = [{ url: 'diegolundstest99@gmail.com' }, { url: 'https://github.com/Decxo360' }, { url: 'https://www.linkedin.com/in/diegolundstedt/' }]
 
 export const RedesSociales = () => {
+  const [invisible, setInvisible] = useState('invisible')
   const [msg, setMsg] = useState("Copiar")
-  const pressClick =()=>{
+  const pressClick = () => {
     navigator.clipboard.writeText(urls[0].url)
-      setMsg(()=>'¡Copiado!')
+    setMsg(() => '¡Copiado!')
   }
+  const visible = () => {
+    console.log(invisible);
+    invisible == 'invisible' ? setInvisible('visible') : setInvisible('invisible')
+  }
+
+
+
   return (
-    <div className='flex flex-col justify-center items-center'>
-        <h1 className='mt-5 text-[30px] text-zinc-200'> ¿Dónde me encuentras? </h1>
-        <div className='flex flex-col justify-around items-center mt-7 w-[100%] 2xl:flex-row xl:flex-row lg:flex-row md:flex-row sm:flex-row'>
-          <div className='mt-3 mb-10 flex flex-col rounded-xl justify-center items-center w-[150px] h-[150px] shadow-2xl shadow-zinc-600 hover:cursor-pointer hover:shadow-blue-700 ' onClick={pressClick}>
-            <TooltipComponent>
-              <Gmail/>
-              <span className='tooltipText'>{msg}</span>
-            </TooltipComponent>
-            <span className='text-slate-200'>Mi correo</span>
+    <div className='fixed bottom-[20px] right-[20px] '>
+      <div className=' flex flex-col-reverse invisible rounded-full shadow-2xl shadow-zinc-600 hover:cursor-pointer hover:shadow-blue-700 '>
+        <div>
+          <TooltipComponent> 
+            <div className={`${invisible} flex justify-center items-center rounded-full w-[80px] h-[80px]  mb-10  shadow-2xl shadow-zinc-600 hover:cursor-pointer hover:shadow-blue-700`} onClick={pressClick}>
+            <Gmail />
           </div >
-           <a className=' mt-3 mb-10 flex flex-col rounded-2xl justify-center items-center w-[150px] h-[150px] shadow-2xl shadow-zinc-600 hover:shadow-blue-700' href={urls[1].url}>
-            <TooltipComponent>
-              <Github/>
-              <span className='tooltipText'>Ir</span>
-            </TooltipComponent>
-            <span className='text-slate-200'>Mi Github</span>
-           </a>
-          <a className='mt-3 mb-10 flex flex-col rounded-2xl justify-center items-center w-[150px] h-[150px] shadow-2xl shadow-zinc-600 hover:shadow-blue-700' href={urls[2].url}>
-            <TooltipComponent>
-              <Linkedin/>
-              <span className='tooltipText'>Ir</span>
-            </TooltipComponent>
-            <span className='text-slate-200'>Mi Linkedin</span>
-          </a>
-          <a download={'DiegoLundstedtCv'} className='mt-3 mb-10 flex flex-col rounded-2xl justify-center items-center w-[150px] h-[150px] shadow-2xl shadow-zinc-600 hover:shadow-blue-700' href={'https://drive.google.com/file/d/12H-RbyrqxAc3ep42y7P-t6F6SjMWfX7t/view?usp=share_link'}>
-            <TooltipComponent>
-              <img className='w-[50px]' src="https://cdn-icons-png.flaticon.com/512/942/942833.png" alt="cv" />
-              <span className='tooltipText'>Ver CV</span>
-            </TooltipComponent>
-            <span className='text-slate-200'>Mi CV</span>
-          </a>
+            <span className='tooltipText'> Mi correo </span>
+          </TooltipComponent>
         </div>
+        <div>
+          <TooltipComponent>
+            <a className={`${invisible} flex justify-center items-center rounded-full w-[80px] h-[80px]  mb-10 shadow-2xl shadow-zinc-600 hover:shadow-blue-700`} href={urls[1].url}>
+              <Github />
+            </a>
+            <span className='tooltipText'> Mi Github</span>
+          </TooltipComponent>
+        </div>
+        <div>
+          <TooltipComponent>
+            <a className={`${invisible} flex justify-center items-center rounded-full w-[80px] h-[80px]  mb-10 shadow-2xl shadow-zinc-600 hover:shadow-blue-700`} href={urls[2].url}>
+              <Linkedin />
+            </a>
+            <span className='tooltipText'> Mi Linkedin </span>
+          </TooltipComponent>
+        </div>
+        <div>
+          <TooltipComponent>
+          <a download={'DiegoLundstedtCv'} className={`${invisible} flex justify-center items-center  mb-10  rounded-full w-[80px] h-[80px] shadow-2xl shadow-zinc-600 hover:shadow-blue-700`} href={'https://drive.google.com/file/d/12H-RbyrqxAc3ep42y7P-t6F6SjMWfX7t/view?usp=share_link'}>
+            <Cv></Cv>
+          </a>
+          <span className='tooltipText'>Mi cv</span>
+          </TooltipComponent>
+        </div>
+        <div>
+          <TooltipComponent>
+            <div className={`${invisible} rotate-180 flex justify-center items-center rounded-full w-[80px] h-[80px]  mb-10 shadow-2xl shadow-zinc-600 hover:shadow-blue-700`}>
+              <img className='animate-bounce ' width={43} src="https://cdn-icons-png.flaticon.com/512/608/608258.png" alt=" flecha" />
+            </div>
+            <span className='tooltipText'>Subir</span>
+          </TooltipComponent>
+          
+        </div>
+      </div>
+      <div>
+        <TooltipComponent>
+          <div className='flex justify-center items-center rounded-full w-[80px] h-[80px] shadow-2xl shadow-zinc-600 hover:cursor-pointer hover:shadow-blue-700 ' onClick={visible}>
+            <img width={43} src="https://cdn.icon-icons.com/icons2/1464/PNG/512/cell_100187.png" alt=" cruz" />
+          </div >
+          <span className='tooltipText'> Opciones</span>
+        </TooltipComponent>
+      </div>
+     
     </div>
   )
 }
