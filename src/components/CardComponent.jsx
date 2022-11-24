81d6f2065201d8abb2e7681e3d1c93855b09c7d1
Decxo360 <diegolundstedt99@gmail.com>
2022-11-24 19:39:27 -0300

    Adicion de estilos

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -3,20 +3,23 @@ import { ButtomComponent } from './ButtomComponent'
 
 export const CardComponent = ({titulo,etiquetas,descripcion,url,codigo}) => {
   return (
-    <div className='flex flex-col w-[500px] mt-8   ml-10 mr-10 bg-slate-800 rounded-md hover:shadow-lg hover:shadow-blue-500/50'>
+    <div className='flex flex-col w-[500px] mt-8   ml-10 mr-10 bg-slate-800 rounded-md hover:shadow-lg hover:shadow-blue-500/50 animate__animated animate__slideInLeft'>
         <div className='mb-4 flex justify-center items-center bg-slate-300 rounded-md'>
             <img src={url} alt="web" className='w-[400px] mt-3 mb-3' />
         </div>
         <div >
-        <h1 className='flex justify-center items-center text-[40px] text-white'>{titulo}</h1>
+        <h1 className='flex justify-center items-center text-[40px] text-white '>{titulo}</h1>
             <div className='grid grid-cols-1 justify-around ml-4 mr-10 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3'> 
                 {etiquetas.map((etiqueta,index)=>(
                     <ButtomComponent key={index}>{etiqueta}</ButtomComponent>
                 ))}
             </div>
-            <div className='ml-10 mr-10 mt-3 mb-8 flex flex-col justify-center items-center text-white text-justify'>
+            <div className='ml-10 mr-10 mb-20 mt-5 flex flex-col justify-center items-center text-white text-justify'>
                 <p>{descripcion}</p>
-                <button className='mt-5 bg-indigo-800 rounded-md h-[35px] w-[100px] hover:scale-125 ease-in-out duration-100 delay-150 hover:bg-indigo-500 '><a href={codigo}>Ver código</a></button>            </div>
+            </div>
+            <div className='absolute bottom-5 left-[40%] text-white h-[50px]' >
+                <button className='mt-5 bg-indigo-800 rounded-md h-[35px] w-[100px] hover:scale-125 ease-in-out duration-100 delay-150 hover:bg-indigo-500 '><a href={codigo}>Ver código</a></button>
+            </div>
         </div>
     </div>
   )
