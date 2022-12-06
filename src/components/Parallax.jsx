074787dd753b1fb40eb0a9abe80f6167dbafb876
Decxo360 <diegolundstedt99@gmail.com>
2022-12-06 18:43:51 -0300

    efecto parallax terminado

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 export const Parallax = () => {
+
+
   return (
     <div className='w-[100%] h-[100vh] mt-[-80px]'>
-        <img className='opacity-50 absolute z-[-1]' style={{width:'100%', height:'100vh'}} src="https://i0.wp.com/academy.leewayweb.com/wp-content/uploads/2018/07/cursos-y-consejos-para-aprender-a-programar-desde-cero-gratis-y-online.jpg?w=996&ssl=1" alt="programming" />
+        <img className='opacity-50  fixed z-[-1] w-[100%] h-[100vh]' src="https://i0.wp.com/academy.leewayweb.com/wp-content/uploads/2018/07/cursos-y-consejos-para-aprender-a-programar-desde-cero-gratis-y-online.jpg?w=996&ssl=1" alt="programming" />
         <div className='flex flex-col justify-center items-center w-[100%] h-[100vh] '>
             <h1 className='text-black text-[50px] text-center'>Analista Programador</h1>
             <span className='text-[20px]'>Diego Ignacio Lundstedt Muñoz</span>
