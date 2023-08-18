41289f4fc41fa4e79f441eb57fbdcf081b911e14
Decxo360 <diegolundstedt99@gmail.com>
2023-08-17 22:29:09 -0400

    refactor

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,8 @@
 import {gsap} from 'gsap'
-export const NavBar = ({inicio,aboutMe,competencias,proyecto}) => {
+
+const pStyle='ml-4 text-white text-[20px] transition ease-in-out duration-700 hover:scale-125 cursor-pointer'
+
+export const NavBar = ({inicio,aboutMe,competencias,proyecto,contacto}) => {
 
   const scrollTo = (elementRef)=> {
     window.scrollTo({
@@ -11,9 +14,10 @@ export const NavBar = ({inicio,aboutMe,competencias,proyecto}) => {
   return (
     <div className='h-[60px] flex justify-center items-center w-full  ' ref={inicio}>
         <div className='flex flex-row justify-center items-center'>
-            <p className='ml-4 text-white text-[20px] transition ease-in-out duration-700 hover:scale-125 cursor-pointer' onClick={()=> scrollTo(aboutMe)}>Sobre mí</p>
-            <p className='ml-4 text-white text-[20px] transition ease-in-out duration-700 hover:scale-125 cursor-pointer' onClick={()=>  scrollTo(competencias)}>Competencias</p>
-            <p className='ml-4 text-white text-[20px] transition ease-in-out duration-700 hover:scale-125 cursor-pointer' onClick={()=>  scrollTo(proyecto)}>Proyectos</p>
+            <p className={pStyle} onClick={()=> scrollTo(aboutMe)}>Sobre mí</p>
+            <p className={pStyle} onClick={()=>  scrollTo(competencias)}>Competencias</p>
+            <p className={pStyle} onClick={()=>  scrollTo(proyecto)}>Proyectos</p>
+            <p className={pStyle} onClick={()=>  scrollTo(contacto)}>Contactame</p>
         </div>
     </div>
   )
